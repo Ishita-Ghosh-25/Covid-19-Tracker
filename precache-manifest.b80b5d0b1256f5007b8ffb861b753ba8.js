b0d58114b0ec9dd5e6516e341c39c3f243dc5333
Ishita Ghosh <ishita.ghosh251999@gmail.com>
2020-11-03 00:49:34 +0530

    Updates

---
+++ precache-manifest.b80b5d0b1256f5007b8ffb861b753ba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e573491996a8e672a4381dc0493dcf",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "3c5e441f1abc04e22fa9",
+    "url": "/Covid-19-Tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "ca657a689df651fbd284",
+    "url": "/Covid-19-Tracker/static/css/main.cababe43.chunk.css"
+  },
+  {
+    "revision": "3c5e441f1abc04e22fa9",
+    "url": "/Covid-19-Tracker/static/js/2.c7e27843.chunk.js"
+  },
+  {
+    "revision": "ed6b993532326238a25c68b99337d08d",
+    "url": "/Covid-19-Tracker/static/js/2.c7e27843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca657a689df651fbd284",
+    "url": "/Covid-19-Tracker/static/js/main.0147f6fc.chunk.js"
+  },
+  {
+    "revision": "3393972647a74dc3f87b",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.01c7af4f.js"
+  }
+]);
